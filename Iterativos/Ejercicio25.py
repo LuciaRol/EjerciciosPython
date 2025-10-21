19b4733d61cd51924ea0b49ce7d553fb32794f5d
Lucía Rol <luciarol87@gmail.com>
2025-10-21 17:30:16 +0200

    Último ejercicio de bucles

--- Iterativos/Ejercicio25.py
+++ Iterativos/Ejercicio25.py
@@ -8,4 +8,47 @@ Fecha: 21/10/2025
 """
 
 def numeros():
-    """ ... """
+    """Pide números hasta que se introduce un negativo.
+    Calcula la cantidad de números introducidos, la media de los impares 
+    y el mayor de los pares.
+    """
+    i = 0
+    suma_impares = 0
+    cantidad_impares = 0
+    mayor_par = None 
+
+    num = int(input("Introduce un número (introduce un número entero negativo para terminar): "))
+
+    while num >= 0:
+        i += 1
+
+        if num % 2 == 0: 
+            if mayor_par is None or num > mayor_par:
+                mayor_par = num
+        else: 
+            suma_impares += num
+            cantidad_impares += 1
+
+        num = int(input("Introduce otro número: "))
+
+  
+    mensaje = f"Cantidad de números introducidos: {i}"
+
+    if cantidad_impares > 0:
+        media_impares = suma_impares / cantidad_impares
+        # Al poner += hace que los mensajes posteriores no sobreescriban a los anteriores, es decir, se añaden
+        mensaje += f"Media de los números impares: {media_impares:.2f}"
+    else:
+        mensaje += "No se introdujeron números impares."
+
+    if mayor_par is not None:
+        mensaje += f"El mayor de los números pares es: {mayor_par}"
+    else:
+        mensaje += "No se introdujeron números pares."
+
+    return mensaje
+
+
+print(numeros())
+
+
